e28f122ec9467c5e8439d239e9b617b430e78a0e
Tumanov Vycheslav <63115274+NoXis82@users.noreply.github.com>
2025-02-10 22:02:35 +0300

    add ProfileScreen

---
+++ app/src/main/java/ru/noxis/composeuitemplates/features/profile_ui/data/Posts.kt
@@ -0,0 +1,48 @@
+package ru.noxis.composeuitemplates.features.profile_ui.data
+
+data class Posts(
+    val imageUrl: String,
+    val text: String
+)
+
+val posts_list = listOf(
+    Posts(
+        "https://bit.ly/3AgPBVI",
+        "gojo and itadori"
+    ),
+    Posts(
+        "https://bit.ly/3oAIk0M",
+        "domain expansion"
+    ),
+    Posts(
+        "https://bit.ly/3AcqrYy",
+        "gojo googles"
+    ),
+    Posts(
+        "https://bit.ly/3BgosU6",
+        "gojo"
+    ),
+    Posts(
+        "https://bit.ly/3BdkZ97",
+        "hanging around with friend"
+    ),
+    Posts(
+        "https://bit.ly/3Acr702",
+        "careless"
+    ),
+    Posts(
+        "https://bit.ly/3mtCMT8",
+        "young gojo"
+    )
+)
+
+val tag_posts_list = listOf(
+    Posts(
+        "https://bit.ly/3abA6Uq",
+        "gojo cosplay"
+    ),
+    Posts(
+        "https://bit.ly/3DhIUEG",
+        "gojo art"
+    )
+)
