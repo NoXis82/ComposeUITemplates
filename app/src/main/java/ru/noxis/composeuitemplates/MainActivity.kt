b176d65a34f300c5ca4548012b9f0f59593a78ef
Tumanov Vycheslav <63115274+NoXis82@users.noreply.github.com>
2025-02-10 20:20:37 +0300

    add MainScreen

--- app/src/main/java/ru/noxis/composeuitemplates/MainActivity.kt
+++ app/src/main/java/ru/noxis/composeuitemplates/MainActivity.kt
@@ -5,12 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import ru.noxis.composeuitemplates.features.mainscreen.Navigation
 import ru.noxis.composeuitemplates.ui.theme.ComposeUITemplatesTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +18,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeUITemplatesTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    val navController = rememberNavController()
+                    Navigation(navController, innerPadding)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ComposeUITemplatesTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/ru/noxis/composeuitemplates/features/mainscreen/Screen.kt
@@ -0,0 +1,19 @@
+package ru.noxis.composeuitemplates.features.mainscreen
+
+sealed class Screen(val route: String) {
+    object MainScreen: Screen("Main_screen")
+    object ChatScreen: Screen("Chat_screen")
+    object ProfileScreen: Screen("Profile_screen")
+    object ProfileScreen2: Screen("Profile_screen_2")
+    object EcommerceScreen: Screen("ecommerce_screen")
+    object EcommerceProductScreen: Screen("ecommerce_product_screen")
+    object SignupScreen: Screen("signup_screen")
+    object LoginScreen: Screen("login_screen")
+    object PaymentScreen:Screen("payment_screen")
+    object BottomNavigationScreen: Screen("bottom_navigation_screen")
+}
+
+sealed class PaymentScreens(val route: String){
+    object AmountScreen:PaymentScreens("payment_amount")
+    object SuccessScreen:PaymentScreens("payment_success")
+}
