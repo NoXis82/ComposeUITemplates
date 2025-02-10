cbad58ea267acc791ccf104d30f50877222c9468
Tumanov Vycheslav <63115274+NoXis82@users.noreply.github.com>
2025-02-10 20:02:03 +0300

    add all dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.parcelize)
 }
 
 android {
@@ -49,6 +50,16 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
+    implementation(libs.navigation)
+    implementation(libs.navigation.compose)
+
+    implementation(libs.coil.compose)
+
+    implementation(libs.material.icons.extended)
+
+    implementation(libs.constraintlayout.compose)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
